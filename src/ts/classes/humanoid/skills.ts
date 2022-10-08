4059028568001a7ec24119136b871b67848f06c9
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2022-10-08 13:40:13 +0800

    skills

---
+++ src/ts/classes/humanoid/skills.ts
@@ -0,0 +1,10 @@
+export interface Skills {
+    strength: number
+    electrical: number
+    mechanical: number
+    fortitude: number
+    luck: number
+    agility: number
+    operations: number
+    intelligence: number
+}
