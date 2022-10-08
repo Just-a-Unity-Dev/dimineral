f4240ba361e37fd101ebdb389793865d90aef184
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2022-10-08 13:40:25 +0800

    basic hello world

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,4 +1,7 @@
 import '../css/style.css'
+import { Part } from './classes/ship/part';
+import { Ship } from './classes/ship/ship';
+import { DamageSpecifier, Health } from './classes/humanoid/health';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <div>
