28e9034be7e4da0e6dfd53374648a0e80537b8fc
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2022-10-08 13:51:22 +0800

    crew support

--- src/ts/classes/ship/ship.ts
+++ src/ts/classes/ship/ship.ts
@@ -1,20 +1,28 @@
+import { Character } from '../humanoid/character';
 import { Part } from './part';
 
 export class Ship {
-    parts: Part[] = []
-    constructor(parts: Part[]) {
-        this.parts = parts
+    public parts: Part[] = [];
+    public crew: Character[] = [];
+
+    constructor(parts: Part[], crew: Character[]) {
+        this.parts = parts;
+        this.crew = crew;
     }
 
     public addPart(part: Part) {
-        this.parts.push(part)
+        this.parts.push(part);
     }
 
     public getPartById(id: string) {
-        return this.parts.find(part => part.id == id)
+        return this.parts.find(part => part.id == id);
     }
 
-    public totalHealth(this: Ship, verbose: boolean = false) {
+    public getCrewByName(name: string) {
+        return this.crew.find(member => member.name == name);
+    }
+
+    public totalHull(this: Ship, verbose: boolean = false) {
         let hp = 0;
         let shield = 0;
         this.parts.forEach(part => {
