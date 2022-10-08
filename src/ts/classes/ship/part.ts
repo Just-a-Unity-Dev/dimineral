2b5b7efb24d31b6732535136d5c6f9cbb810e454
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2022-10-08 22:06:30 +0800

    parts

--- src/ts/classes/ship/part.ts
+++ src/ts/classes/ship/part.ts
@@ -38,13 +38,42 @@ export class Part {
      * Returns the total health (including shield)
      * and if you want it to be verbose you can mark it as such.
      * @param verbose boolean
-     * @returns number
+     * @returns number | string
     */
-    public totalHealth(verbose: boolean = false) {
+    public totalHealth(verbose: boolean = false): number | string {
         let total: number = this.health + this.shield;
         if (verbose) {
             return `${this.health} hull + ${this.shield} shield = ${total} total`;
         }
         return total;
+    }
+
+    public updateUi(id: string) {
+        const data = document.getElementById(`${id}-${this.id}-data`);
+        if (data == undefined) return;
+        data.textContent = <string>this.totalHealth(true);
+    }
+
+    /**
+     * Initialize UI
+     * @param id string
+     * @returns Node
+     */
+    public ui(id: string): Node {
+        // Main div
+        const div = document.createElement("div");
+        div.classList.add("item");
+
+        // Data
+        const label = document.createElement("h2");
+        label.textContent = this.name;
+        div.appendChild(label);
+
+        const data = document.createElement("p");
+        data.id = `${id}-${this.id}-data`;
+        data.textContent = <string>this.totalHealth(true);
+        div.appendChild(data);
+
+        return div;
     }
 }
