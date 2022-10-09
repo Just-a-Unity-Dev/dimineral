3124571c63418b3dc6afe7cb3eeb174dbbea11c3
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2022-10-09 17:45:09 +0800

    ship names

--- src/ts/util/ship.ts
+++ src/ts/util/ship.ts
@@ -1,10 +1,14 @@
-import { generateString } from './rng';
+import { generateString, pickFromArray } from './rng';
 import shipNames from "../json/shipNames.json";
 
+/**
+ * Generates a random ship name
+ * @returns string
+ */
 export function generateName(): string {
     let prefix = generateString(2).toUpperCase();
     let id = Math.floor(Math.random() * 100);
-    let name = shipNames[Math.floor(Math.random() * shipNames.length)]
+    let name = pickFromArray(shipNames);
 
     return `${prefix}-${id} ${name}`
 }
