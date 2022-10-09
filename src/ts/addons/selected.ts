83fc48c12397af45d55d4162a45d3f35767bf2fa
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2022-10-09 19:52:10 +0800

    Selection typescript

---
+++ src/ts/addons/selected.ts
@@ -0,0 +1,38 @@
+export let selected: string = "";
+
+export function setSelected(name: string) {
+    selected = name;
+}
+
+/**
+ * Initalizes the selected div
+ */
+export function initSelectedDiv(): Node {
+    let selectedDiv = document.createElement("div");
+
+    // basic name
+    let name = document.createElement("h2");
+    name.textContent = selected;
+    name.id = "selected-name";
+
+    // deselect button
+    let deselect = document.createElement("button");
+    deselect.textContent = "Deselect";
+
+    // this overwrites the previous onclick to prevent repeats
+    deselect.onclick = function () {
+        // deselected
+        setSelected("");
+    }
+    
+    selectedDiv.appendChild(name);
+    selectedDiv.appendChild(deselect);
+    selectedDiv.classList.add("selected-ui");
+    selectedDiv.classList.add("solid");
+
+    // turn it off for now
+    selectedDiv.style.display = "none";
+
+    // done initializing
+    return selectedDiv;
+}
