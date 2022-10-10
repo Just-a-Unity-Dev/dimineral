6016824796c2349403bad77a87df1c8536125f59
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2022-10-10 18:38:46 +0800

    linting

--- src/ts/util/date.ts
+++ src/ts/util/date.ts
@@ -4,8 +4,7 @@
  * @returns string
  */
 export function ordinalSuffixOf(i: number): string {
-    var j = i % 10,
-        k = i % 100;
+    const j = i % 10, k = i % 100;
     if (j == 1 && k != 11) {
         return i + "st";
     }
