7769748a2b0d6e24a66ece68ad46bed4c369941f
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2022-10-10 11:12:48 +0800

    power tests

--- tests/power.spec.ts
+++ tests/power.spec.ts
@@ -1,7 +1,26 @@
 import { describe, expect, it } from 'vitest';
+import { Power } from "../src/ts/addons/ship/power";
 
-describe("characters", () => {
+describe("power", () => {
     it("adding suppliers works", () => {
-        // TODO: Write actual tests
-    })
+        const power: Power = new Power();
+        power.addSupplier("test", 20)
+        expect(power.suppliers).toStrictEqual({"test": 20});
+    });
+    it("adding consumers works", () => {
+        const power: Power = new Power();
+        power.addConsumer("test", 20)
+        expect(power.consumers).toStrictEqual({"test": 20});
+    });
+    it("total works", () => {
+        const power: Power = new Power();
+
+        power.addSupplier("life", 100);
+
+        power.addConsumer("shields", 25);
+        power.addConsumer("break", 25);
+        power.addConsumer("weapons", 25);
+
+        expect(power.power).toStrictEqual(25);
+    });
 })
